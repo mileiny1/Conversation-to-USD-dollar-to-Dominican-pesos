8be2f0ef76dd1da476fdbea0480176d651b3f5da
Mileiny Nolasco <149266977+mileiny1@users.noreply.github.com>
2024-09-23 23:00:28 -0400

    Add files via upload

---
+++ conversation.py
@@ -0,0 +1,9 @@
+#Current Conversation to US Dollar to Dominican Pesos
+
+USD = input ("Enter the amount of the dollar:")
+
+USD = float (USD)
+
+DP = 60*USD
+
+print ("Amount of the Dominican Pesos is %.2f" %DP)
